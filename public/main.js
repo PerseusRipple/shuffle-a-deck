40e19b84322cd37d9950bb7f0a4d25292ff9f6ce
Shantell Smithson <perseus.ripple@gmail.com>
2019-02-06 19:37:33 -0500

    added deck of cards, shuffle and swap

--- public/main.js
+++ public/main.js
@@ -2,20 +2,65 @@ const main = () => {
   if (document.querySelector('.game-name')) {
     document.querySelector('.game-name').textContent = 'Shuffle a Deck!!!'
   }
-}
- 
-const faceUp = []
 
-for (let i = 0; 1 < 1++) {
-  const _li = document.createElement ('li')
-  _li.textContent = faceUp[i];
-  document.querySelector('.cards').appendChild(_li)
-} 
+  let deck = []
 
+  const valueOfCards = [
+    'Ace',
+    'King',
+    'Queen',
+    'Jack',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10'
+  ]
+  const suitsOfCards = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
 
-/*
+  // Build Deck
+
+  for (let values = 0; values < valueOfCards.length; values++) {
+    for (let suits = 0; suits < suitsOfCards.length; suits++) {
+      let value = valueOfCards[values]
+      let suit = suitsOfCards[suits]
+      deck.push('the ' + value + ' of ' + suit)
+    }
+  }
+
+  // Shuffle Deck
+
+  for (let shuffle = 51; shuffle >= 0; shuffle--) {
+    const random = Math.floor(Math.random() * (shuffle + 1))
+
+    // Swap card at position shuffle with the card at position random
+
+    let cardAtPositionShuffle = deck[shuffle]
+    let cardAtPositionRandom = deck[random]
+
+    // Put the card at position shuffle at position random
+    deck[shuffle] = cardAtPositionRandom
+
+    // Put the card at poistion random at position shuffle
+    deck[random] = cardAtPositionShuffle
+
+    console.log(deck)
+  }
+
+  console.log(deck)
+
+  //Deal Deck
+
+  /* document.querySelector('.cards').appendChild(_li) */
+
+  /*
  const mySection = document.createElement('section')
 mySection.textContent = 'Hello, World'
-document.querySelector('.container').appendChild(mySection)
+document.querySelector('.container').appendChild(mySection) */
+}
 
-document.addEventListener('DOMContentLoaded', main) */
+document.addEventListener('DOMContentLoaded', main)
