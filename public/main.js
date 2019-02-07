6766f358e76ad88f6d8e0efa566ecdcf6f481667
Shantell Smithson <perseus.ripple@gmail.com>
2019-02-07 16:56:29 -0500

    added player 2 face up

--- public/main.js
+++ public/main.js
@@ -1,74 +1,84 @@
+let deck = []
+
+const valuesOfCards = [
+  'Ace',
+  'King',
+  'Queen',
+  'Jack',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10'
+]
+const suitsOfCards = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+
 const main = () => {
-  if (document.querySelector('.game-name')) {
-    document.querySelector('.game-name').textContent = 'Shuffle a Deck!!!'
-  }
+  // call function to create deck on page load
+  buildDeck()
+  // call function
+  shuffleDeck()
+}
 
-  let deck = []
-
-  const valuesOfCards = [
-    'Ace',
-    'King',
-    'Queen',
-    'Jack',
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
-    '7',
-    '8',
-    '9',
-    '10'
-  ]
-  const suitsOfCards = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
-
-  // Build Deck
-
-  for (let value = 0; value < valuesOfCards.length; value++) {
-    for (let suits = 0; suits < suitsOfCards.length; suits++) {
-      let values = valuesOfCards[value]
-      let suit = suitsOfCards[suits]
-      deck.push('the ' + values + ' of ' + suit)
+// Build Deck
+const buildDeck = () => {
+  for (let i = 0; i < suitsOfCards.length; i++) {
+    // console.log(deck)
+    for (let j = 0; j < valuesOfCards.length; j++) {
+      // let values = valuesOfCards[value]
+      // let suit = suitsOfCards[suits]
+      deck.push('the ' + valuesOfCards[j] + ' of ' + suitsOfCards[i])
     }
   }
+  console.log(deck)
+}
+// Shuffle Deck
 
-  // Shuffle Deck
-
-  for (let shuffle = 51; shuffle >= 0; shuffle--) {
-    const random = Math.floor(Math.random() * (shuffle + 1))
+const shuffleDeck = () => {
+  for (let i = deck.length - 1; i >= 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1))
 
     // Swap card at position shuffle with the card at position random
 
-    let cardAtPositionShuffle = deck[shuffle]
-    let cardAtPositionRandom = deck[random]
+    let cardAtPositionI = deck[i]
+    let cardAtPositionJ = deck[j]
 
     // Put the card at position shuffle at position random
-    deck[shuffle] = cardAtPositionRandom
+    deck[i] = cardAtPositionJ
 
     // Put the card at position random at position shuffle
-    deck[random] = cardAtPositionShuffle
+    deck[j] = cardAtPositionI
 
-    console.log(deck)
+    // console.log(deck)
   }
-
   console.log(deck)
+}
 
-  // Deal Card
+// console.log(deck)
 
-  const dealCard = () => {
-    let newCard = deck.splice(0, 1)
-   if (deck.length > 0) {
-const _li = document.createElement ('.li')
-_li.textContent= newCard
-document.querySelector('.Cards').appendChild(_li)
-   }
+// Deal Card
 
+const dealCard = () => {
+  let newCard = deck.splice(0, 1)
+  if (deck.length > 0) {
+    // const _li = document.createElement('li')
+    // li.textContent = newCard
+    document.querySelector('.new-card-faceup').textContent = newCard
   }
+  let nextNewCard = deck.splice(0, 1)
 
-  
-  
-  console.log('return')
+  if (deck.length > 0) {
+    // const _li = document.createElement('li')
+    // li.textContent = newCard
+    document.querySelector('.player-1-faceup').textContent = nextNewCard
+  }
+}
 
+// console.log('return')
 
 /* for(let deck = Math.floor(Math.random() * cardAtPositionShuffle.length)
     return cardAtPositionShuffle.splice(card, 1)[0] */
@@ -79,5 +89,5 @@ mySection.textContent = 'Hello, World'
 document.querySelector('.container').appendChild(mySection) */
 
 // i = shuffle
-document.querySelector('#deal').addEventListener('click', dealCard)
-document.EventListener('DOMContentLoaded', main)
+document.addEventListener('DOMContentLoaded', main)
+document.querySelector('.deal').addEventListener('click', dealCard)
